959d11bbc9731f47701a6c7a923af2c96d51a750
Wyatt Simmons <simmonsw182@gmail.com>
2021-06-24 13:55:09 -0700

    implemented closing the modal

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -110,12 +110,14 @@ function Photolist({ category }) {
 
   const toggleModal = (image, i) => {
     setCurrentPhoto({...image, index: i});
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
       <div className='flex-row'>
         {currentPhotos.map((image, i) => (
           <img
