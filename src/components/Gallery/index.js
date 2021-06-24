e5d362300718d44a9167de032ae70b983ba0693f
Wyatt Simmons <simmonsw182@gmail.com>
2021-06-23 19:58:17 -0700

    displayed photos based on selected category

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -10,7 +10,7 @@ function Gallery({ currentCategory }) {
     <section>
       <h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
       <p>{description}</p>
-      <Photolist />
+      <Photolist category={currentCategory.name} />
     </section>
   );
 }
