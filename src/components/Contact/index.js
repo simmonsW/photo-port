925b9a10e51e8c777f952813b3c324fcb521a7b5
Wyatt Simmons <simmonsw182@gmail.com>
2021-06-24 11:42:14 -0700

    validated form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 
   const [ formState, setFormState ] = useState({ name: '', email: '', message: ''});
+  const [ errorMessage, setErrorMessage ] = useState('');
 
   const { name, email, message } = formState
 
   function handleChange(e) {
-    setFormState({...formState, [e.target.name]: e.target.value })
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({...formState, [e.target.name]: e.target.value })
+    }
   }
 
   function handleSubmit(e) {
